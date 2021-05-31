4f55f689abba7fdfbe5b1fe091d985f45784b494
luigizuniga <luigialbertozuniga@gmail.com>
2021-05-31 01:36:43 -0400

    react - Hooks uso de State useState en componentes funcional y de clase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// Propiedades, recordar que son de solo lectura
-// si necesitamos modificar las propiedades debemos hacerlo desde fuera,
-// es por eso que se hace uso del metodo STATE.
-// en este ejemplo se usara un contador que modicara todo el pono
-// estas propiedades seran heredaras desde el metodo render
-
-// se modifica la constante por let ya que esta se ira modificando
 let propiedades = {
   hijo1: {
     titulo: "Hijo 1",
@@ -22,87 +15,137 @@ let propiedades = {
   }
 }
 
-setInterval(() => {
-  console.log('Propiedades Intervalo...');
 
-  propiedades = {
-    ...propiedades,
-    hijo1: {
-      ...propiedades.hijo1,
-      cuenta: propiedades.hijo1.cuenta + 1
-    },
-    hijo2: {
-      ...propiedades.hijo2,
-      cuenta: propiedades.hijo2.cuenta + 10
+function Hijo2(props) {
+  // Declaracion de State - Componente Funcional
+  const [ state, setState ] = useState(
+    {
+      titulo: "Hijo 2",
+      subtitulo: "Contador (Componente de Clase)",
+      cuenta: 10
+    }
+  );
+
+
+  //#region Modificacion de State en un componente Funcional
+  /*
+   * Para modificar el stado solo debemos llamar a la funcion que declaramos
+   * para este tipo de componente no existe this
+  */
+  //#endregion
+  const disminuir = () =>{
+    if(state.cuenta <= 0){
+      setState({
+        ...state,
+        cuenta: 0
+      })
+    }else{
+      setState({
+        ...state,
+        cuenta:state.cuenta - 1
+      })
     }
   }
 
-  // Render se deja dentro de setInterval propage la modificacion de propiedades desde fuera de los componentes
-  // No nos dara error de solo lectura, por que la actualizacion de propiedades viene desde fuera del componente
-  // Evitamos el error de tipo: Cannot assign to read only property 'cuenta' of object '#<Object>'
-  ReactDOM.render(
-    <Padre {...propiedades} />,
-    document.getElementById('root')
-  );
-
-}, 1000);
-
-
-
-
-// Extraccion de division de componetes
-// Hace referencias a que un componente lo puedes dividie en varios componentes hijos,
-// bajando la complejidad, diviendo un componente en varios subcomponentes.
-
-
-// Componente Funcional , con uso de Extraccion
-function Hijo2(props) {
-  // TypeError: Cannot assign to read only property 'cuenta' of object '#<Object>'
-  // setInterval( ()=>{
-  //   console.log('Intervalo...');
-  //   props.cuenta++;
-  // }, 1000);
+  const aumentar =()=>{
+    setState({
+      ...state,
+      cuenta:state.cuenta + 1
+    })
+  }
 
   return (
     <div className="componente">
-      <h2>{props.titulo}</h2>
-      <h3>{props.subtitulo}</h3>
-      <p>{props.cuenta}</p>
+      <h2>{state.titulo}</h2>
+      <h3>{state.subtitulo}</h3>
+      <div className="controles">
+        <span className="control" onClick={ disminuir }>-</span>
+        <span className="control" onClick={ aumentar }>+</span>
+      </div>
+      <p>{state.cuenta}</p>
     </div>
   );
 }
 
-// Componentes de Clase
-// Uso de ECMASCRIPT 6 para hacer uso de las clases
-// uso de Propiedades
 
 class Hijo1 extends React.Component {
-  // Cannot assign to read only property 'cuenta'
-  // aumentarCuenta(){
-  //   setInterval(()=>{
-  //     console.log('Intervalo...')
-  //     this.props.cuenta++;
-  //   }, 1000);
-  // }
+  // Declaracion del State - Fuera del Contructor
+  state = {
+    titulo:'Hijo 1',
+    subtitulo: 'Contador (Componente Funcional)',
+    cuenta: 1
+  };
 
-  render() {
-    // Cannot assign to read only property 'cuenta'
-    // this.aumentarCuenta();
+  constructor(props) {
+    super(props);
+    this.dismunuir = this.dismunuir.bind(this);
+    this.aumentar = this.aumentar.bind(this);
+    //#region Declaracion State - Opcional
 
+    /* Declaracion del state - Opcional dentro del Constructor
+     this.state = {
+     titulo:'Hijo 1',
+     subtitulo: 'Contador (Componente Funcional)',
+     cuenta: 1
+    */
+
+    //#endregion
+   }
+
+   //#region Metodos de Clase
+    /* Metodos de Clase
+     Declaracion de metodos que usara el State
+     * No heredan el this de la clase
+     * Para el uso de los metodos se hace uso de bind(this)
+     * Se recomienda utilozar arrow functional para los metodos
+     */
+    //#endregion
+
+  dismunuir(){
+    if(this.state.cuenta <= 0){
+      this.setState({
+        ...this.state,
+        cuenta: 0
+      })
+    }else{
+      this.setState({
+        ...this.state,
+        cuenta: this.state.cuenta-1
+      });
+    }
+  }
+
+  aumentar=()=>{
+    this.setState({
+      ...this.state,
+      cuenta: this.state.cuenta+1
+    });
+  }
+
+  render(){
     return (
       <div className="componente">
-        <h2>{this.props.titulo}</h2>
-        <h3>{this.props.subtitulo}</h3>
-        <p>{this.props.cuenta}</p>
+        <h2>{this.state.titulo}</h2>
+        <h3>{this.state.subtitulo}</h3>
+        <div className="controles">
+          <span className="control" onClick={ this.dismunuir }>-</span>
+          <span className="control" onClick={ this.aumentar }>+</span>
+          {
+          //#region uso de SetState Opcionar
+          /* <span className="control" onClick={ ()=>{
+            this.state.cuenta++; // No realizar de esta manera utilizar setState
+            this.forceUpdate(); // Esto forza a la actualizacion del componente
+          }}>+</span> */
+            //#endregion
+          }
+        </div>
+        <p>{this.state.cuenta}</p>
       </div>
     );
   }
 }
 
-// Composicion de componentes
-// Se refiere a que un componente puede estar compuesto a uno o mas componentes hijos (subcomponentes)
 function Padre(props) {
-  // Verificacion de las props (objeto json)
   console.log(props)
   return (
     <div className="padre">
@@ -115,3 +158,8 @@ function Padre(props) {
     </div>
   );
 }
+
+ReactDOM.render(
+  <Padre {...propiedades} />,
+  document.getElementById('root')
+);
